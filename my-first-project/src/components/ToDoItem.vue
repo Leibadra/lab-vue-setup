c1a343a79e53c5d10bb72315de6fd67d52061227
Leila Badra <leila.badra@hotmail.com>
2022-09-01 18:35:15 +0200

    resuelto

---
+++ my-first-project/src/components/ToDoItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input type="checkbox" id="todo-item" />
+    <label for="todo-item">My Todo Item</label>
+  </div>
+  <h1> {{ message }} </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'To do Item',
+    };
+  },
+};
+</script>
